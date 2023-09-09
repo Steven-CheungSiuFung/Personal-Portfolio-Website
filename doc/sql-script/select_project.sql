a335b0e5ab7e1456bbfcf8ef0eb0d3febf330056
Steven Cheung <crsiufung@hotmail.com>
2023-09-09 18:46:13 +0800

    migrate database to Vercel Postgres (getAllProjectsPreview)

--- doc/sql-script/select_project.sql
+++ doc/sql-script/select_project.sql
@@ -1,2 +1,5 @@
 -- getProjectsPreview
 SELECT id, name, description, image, tech FROM project_main WHERE selected=true;
+
+-- getAllProjectsPreview
+SELECT id, name, description, image, tech FROM project_main;
